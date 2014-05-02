93468078e9f8be398b3ff138c845a1508e03dc83
kirkouimet <kirk@kirkouimet.com>
2014-05-01 23:00:57 -0600

    More documentation

--- test.js
+++ test.js
@@ -38,22 +38,27 @@ Function.run = function(generator) {
 			return next.value;
 		}
 
-		// Keep track of our value
+		// Keep track of our current value
 		var value = (next === undefined) ? null : next.value;
 
 		// Move to the next yield or return
-		next = generator.next(value);
-		console.log('Function.pump next:', next);
+		next = generator.next(value); // Pass the value into the generator so it can be assigned
+		console.log('Function.run.pump next:', next);
 
-		// Handle promises
+		// Handle promises by not calling generator.next() until the promise resolves
 		if(next.value instanceof Promise) {
 			console.log('We have a promise we need to wait for!');
 			next.value.done(function(value) {
 				console.log('The promise has fulfilled its duty to mankind. It returned:', value);
+				
+				// Set next.value to the value from the finished promise
 				next.value = value;
+
+				// Tell the generator to move forward
 				pump(generator, next);
 			});
 		}
+		// If we don't have a promise, keep moving the generator forward
 		else {
 			return pump(generator, next);	
 		}
