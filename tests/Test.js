ed917d6600b46310229dee0a93ccd828781278d7
kirkouimet <kirk@kirkouimet.com>
2014-11-02 13:58:53 -0700

    Adding another array test

--- tests/Test.js
+++ tests/Test.js
@@ -1,14 +1,8 @@
-// Usage
-// "Test.js" - find all of the Tests in the tests folder and run them all
-// "Test.js" types" - run all of the tests in tests/types
-// "Test.js" types/ArrayTest" - run all of the tests for ArrayTest
-// "Test.js" types/ArrayTest testContains" - run the testContains test in ArrayTest
-
-// Framework
+// Require Framework and instantiate an empty project
 require('./../Framework.js');
 Project = new Framework();
 
-// Initialize the test module
+// Load and initialize the test module
 Module.load('Test');
 Module.initialize('Test');
 
--- tests/types/ArrayTest.js
+++ tests/types/ArrayTest.js
@@ -3,6 +3,11 @@ ArrayTest = Test.extend({
 	is: function() {
 		Assert.true(Array.is([]));
 		Assert.false(Array.is('[]'));
+		Assert.true(Array.is('string'.toArray()));
+	},
+
+	merge: function() {
+		Assert.equal(Json.encode([1,2,3]), Json.encode([1].merge([2,3])));
 	},
 
 });
