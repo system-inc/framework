95812bc99a9b01a45bb7d401e41ec3ca6c2f50d1
Kirk Ouimet <kirk.ouimet@snapchat.com>
2015-06-29 17:07:25 -0700

    More work on web components

--- modules/web/components/WebComponent.js
+++ modules/web/components/WebComponent.js
@@ -1,17 +1,24 @@
 WebComponent = Class.extend({
 
+	// Every web component must have an HtmlElement
 	element: null,
 
+	// Settings for the web component
 	settings: null,
 
 	construct: function(settings) {
-		this.settings = new Settings(settings);
-
-		// The default tag for a web component is a div
-		this.settings.default({
+		this.settings = Settings.default({
+			// The default tag for a web component is a div
 			tag: 'div',
-		});
+		}, settings);
 
+		// Initialize the component when the DOM document is ready
+		HtmlDocument.on('ready', function() {
+			this.initialize();
+		}.bind(this));
+	},
+
+	initialize: function() {
 		// Every web component must have an HtmlElement
 		this.element = new HtmlElement(this.settings.get('tag'));
 	},
@@ -21,3 +28,11 @@ WebComponent = Class.extend({
 	},
 
 });
+
+// Static methods
+WebComponent.load = function(webComponentName) {
+	var webComponentPath = Project.directory+'views'+Node.Path.separator+'web-components'+Node.Path.separator+webComponentName+'WebComponent.js';
+	//console.log(webComponentPath);
+
+	return Framework.require(webComponentPath);
+}
