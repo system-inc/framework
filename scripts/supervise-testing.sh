62929c097689babd83707c14d66b09efad29221b
kirkouimet <kirk@kirkouimet.com>
2015-07-02 09:04:22 -0700

    Making scripts more robust, not finished entirely yet

--- scripts/supervise-testing.sh
+++ scripts/supervise-testing.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
-BASEDIR=$(dirname $0)
-node --harmony $BASEDIR/../tests/Test.js "-s"
+
+# Get the directory of the script
+scriptInvocationBasename=`basename $0`
+if [ "`echo $0 | cut -c1`" = "/" ]; then
+	scriptPath=`dirname $0`
+else
+	scriptPath=`pwd`/`echo $0 | sed -e s/$scriptInvocationBasename//`
+fi
+scriptDirectory=$(echo $scriptPath | sed -e 's/\\/\//g')
+scriptDirectory=`dirname "$scriptDirectory"`
+#echo $scriptDirectory
+
+# Run the script
+node --harmony "$scriptDirectory/../tests/Test.js" "-s"
