f207ae248043051f4058fcabd7966a75622e0a4d
Kirk Ouimet <kirkouimet@California.local>
2016-01-15 17:08:29 -0800

    Adding convenience methods for getting and setting values

--- modules/web/components/InputWebComponent.js
+++ modules/web/components/InputWebComponent.js
@@ -1,2 +1,13 @@
 InputWebComponent = WebComponent.extend({
+
+	getValue: function() {
+		return this.element.domElement.value;
+	},
+
+	setValue: function(value) {
+		this.element.domElement.value = value;
+
+		return this.getValue();
+	},
+
 });
