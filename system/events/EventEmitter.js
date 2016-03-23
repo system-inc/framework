02df360b6c6109b60c4956a48524946921960187
kirkouimet <kirk@kirkouimet.com>
2016-03-22 22:28:56 -0700

    More work on EventEmitter

--- system/events/EventEmitter.js
+++ system/events/EventEmitter.js
@@ -1,11 +1,43 @@
 // Class
 var EventEmitter = Class.extend({
 
+	/* {
+			eventIdentifier: {
+				listeners: [], array of functions to evoke
+			},
+		}
+	*/
+	events: {},
+
+	maximumListeners: EventEmitter.defaultMaximumListeners,
+
 	construct: function() {
 
 	},
 
+	on: function(eventIdentifier, callback) {
+		if(this.events[eventIdentifier]) {
+			this.events[eventIdentifier].listeners = 
+		}
+	},
+
+	emit: function() {
+
+	},
+
+	once: function() {
+
+	},
+
+	setMaximumListeners: function(maximumListeners) {
+
+	},
+
 });
+
+// Static properties
+
+EventEmitter.defaultMaximumListeners = 10;
 
 // Export
 module.exports = EventEmitter;
