bbb810bc8bd21a60e5809c7110490cd7a7b8fccd
kirkouimet <kirk@kirkouimet.com>
2016-04-01 09:14:43 -0700

    More work and tests for events

--- system/events/EventEmitter.js
+++ system/events/EventEmitter.js
@@ -1,81 +1,178 @@
 // Dependencies
 var Event = Framework.require('system/events/Event.js');
-var EventListener = Framework.require('system/events/EventListener.js');
 
 // Class
 var EventEmitter = Class.extend({
 
 	/*
-		{
-			eventIdentifer1: EventListener,
-			eventIdentifer2: EventListener,
+		[
+			{
+				eventPattern: ,
+				boundFunction: ,
+				timesToRun: ,
+				timesRan: ,
+			},
 			...
-		},
+		]
 	*/
-	eventListeners: {},
+	eventListeners: [],
 
-	maximumListeners: null,
+	recommendedMaximumEventListenersPerEventIdentifier: null,
 
 	construct: function() {
-		this.maximumListeners = EventEmitter.defaultMaximumListeners;
+		// Set the default recommended maximum bound functions per event pattern 
+		this.recommendedMaximumEventListenersPerEventIdentifier = EventEmitter.defaultRecommendedMaximumEventListenersPerEventIdentifier;
 	},
 
 	emit: function*(eventIdentifier, data) {
 		//Console.log('EventEmitter.emit', 'eventIdentifier', eventIdentifier, 'data', data);
 
-		if(this.eventListeners[eventIdentifier] !== undefined) {
-			yield this.eventListeners[eventIdentifier].runCallbacks(data);
+		var matchingEventListeners = this.getEventListeners(eventIdentifier);
+
+		// Need to check counts
+		//// Loop through all callbacks synchronously backwards (so we can edit the array in place)
+		//for(var currentListenersIndex = this.eventListeners.length; currentListenersIndex > -1; currentListenersIndex--) {
+		//	var currentEventListenerObject = this.eventListeners[currentListenersIndex];
+
+		//	// Run the function
+		//	var result = yield boundFunctionObject.boundFunction(event);
+		//	//Console.log('boundFunctionObject result', result);
+
+		//	// Count the run
+		//	boundFunctionObject.timesRan++;
+
+		//	// Unbind the function if the function has been run too many times
+		//	if(boundFunction.timesRan == boundFunctionObject.timesToRun) {
+		//		this.unbindFunctionByListenersIndex(currentListenersIndex);
+		//	}
+		//}
+
+		// Create the event to emit
+		var event = new Event(eventIdentifier, data);
+
+		// If there are no event listeners for an Error, throw the Error
+		if(matchingEventListeners.length == 0 && Error.is(data)) {
+			throw data;
 		}
+		// Run the matching bound functions in sequence
+		else {
+			yield matchingEventListeners.each(function*(matchingEventListenerIndex, matchingEventListenerObject) {
+				yield matchingEventListenerObject.boundFunction(event);
+			});	
+		}		
+
+		return this;
 	},
 
-	on: function(eventIdentifier, callback, options) {
-		return this.addListener(eventIdentifier, callback, options);
-	},
+	getEventListeners: function(eventPattern) {
+		// Create a place to store all of the matching bound function objects
+		var matchingEventListeners = [];
 
-	once: function(eventIdentifier, callback, options) {
-		return this.addListener(eventIdentifier, callback, options);
-	},
-
-	addListener: function(eventIdentifier, callback, options) {
-		//Console.log('EventEmitter.addListener', 'eventIdentifier', eventIdentifier, 'callback', callback, 'options', options);
-
-		// List the event identifier in the list of events
-		if(this.eventListeners[eventIdentifier] === undefined) {
-			this.eventListeners[eventIdentifier] = new EventListener(this, eventIdentifier);
+		// Return all bound functions if there is no event pattern provided
+		if(!eventPattern) {
+			matchingEventListeners = this.eventListeners;
+		}
+		else {
+			// Gather all of the matching bound function objects
+			this.eventListeners.each(function(boundFunctionIndex, boundFunctionObject) {
+				// If the event pattern is a string with a * or if the event pattern is a regular expression
+				if((String.is(boundFunctionObject.eventPattern) && boundFunctionObject.eventPattern.contains('*')) || RegularExpression.is(boundFunctionObject.eventPattern)) {
+					// TODO:
+					//matchingEventListeners.append(boundFunctionObject);
+				}
+				// The event pattern is just a regular string and is meant to be taken literally
+				else if(boundFunctionObject.eventPattern == eventPattern) {
+					matchingEventListeners.append(boundFunctionObject);
+				}
+			});
 		}
 
-		this.eventListeners[eventIdentifier].addCallback(callback, options);
+		return matchingEventListeners;
 	},
 
-	removeListener: function() {
+	bind: function(eventPattern, functionToBind, timesToRun) {
+		//Console.log('EventEmitter.bind', 'eventPattern', eventPattern, 'functionToBind', functionToBind, 'timesToRun', timesToRun);
 
-	},
-
-	removeAllListeners: function() {
-
-	},
-
-	setMaximumListeners: function(maximumListeners) {
-		this.maximumListeners = maximumListeners;
-
-		return this.maximumListeners;
-	},
-
-	getEventIdentifiers: function() {
-		var eventIdentifiers = [];
-
-		this.eventListeners.each(function(eventIdentifier, eventListener) {
-			eventIdentifiers.append(eventIdentifier);
+		this.eventListeners.append({
+			eventPattern: eventPattern,
+			boundFunction: functionToBind,
+			timesToRun: timesToRun,
+			timesRan: 0,
 		});
 
-		return eventIdentifiers;
+		var eventListeners = this.getEventListeners(eventPattern);
+
+		// Check to see if the matching bound function objects is greater than the recommended
+		if(eventListeners.length > this.recommendedMaximumEventListenersPerEventIdentifier) {
+			Console.warn('Possible memory leak detected. There are '+eventListeners.length+' event listeners bound to the event pattern "'+eventPattern+'". The recommended maximum event listeners for this event pattern is '+this.recommendedMaximumEventListenersPerEventIdentifier+'.', "\n"+(new Error().stack.stackTraceToString()));
+		}
+
+		return this;
+	},
+
+	on: function(eventPattern, functionToBind, timesToRun) {
+		return this.bind(eventPattern, functionToBind, timesToRun);
+	},
+
+	once: function(eventPattern, functionToBind) {
+		return this.bind(eventPattern, functionToBind, 1);
+	},
+
+	unbind: function(eventPattern, functionToUnbind) {
+		// If we are unbinding a specific function for an event pattern
+		if(functionToUnbind) {
+			// Walk backwards through the array so we can edit the array in place
+			for(var currentEventListenerIndex = this.eventListeners.length - 1; currentEventListenerIndex >= 0; currentEventListenerIndex--) {
+				var currentEventListener = this.eventListeners[currentEventListenerIndex];
+				//Console.info('currentEventListener', currentEventListener);
+
+				// If the event pattern matches and the current bound function strictly matches
+				if(currentEventListener.eventPattern == eventPattern && functionToUnbind === currentEventListener.boundFunction) {
+					this.eventListeners.delete(currentEventListenerIndex);
+				}
+			}
+		}
+		// If we are unbinding all functions for a given event pattern
+		else {
+			// Walk backwards through the array so we can edit the array in place
+			for(var currentEventListenerIndex = this.eventListeners.length - 1; currentEventListenerIndex >= 0; currentEventListenerIndex--) {
+				var currentEventListener = this.eventListeners[currentEventListenerIndex];
+				//Console.info('currentEventListener', currentEventListener);
+
+				// If the event pattern matches
+				if(currentEventListener.eventPattern == eventPattern) {
+					this.eventListeners.delete(currentEventListenerIndex);
+				}
+			}
+		}
+
+		return this;
+	},
+
+	unbindAll: function() {
+		this.eventListeners = [];
+
+		return this;
+	},
+
+	setRecommendedMaximumListenersPerEventIdentifier: function(recommendedMaximumEventListenersPerEventIdentifier) {
+		this.recommendedMaximumEventListenersPerEventIdentifier = recommendedMaximumEventListenersPerEventIdentifier;
+
+		return this.recommendedMaximumEventListenersPerEventIdentifier;
 	},
 
 });
 
 // Static properties
 
-EventEmitter.defaultMaximumListeners = 10;
+/*
+	By default, the recommended maximum of functions that can be bound to any single event is 10. This recommended limit can be changed for individual
+	EventEmitter instances using the eventEmitter.setRecommendedMaximumListenersPerEventIdentifier(n) method. To change the default for all EventEmitter
+	instances, the EventEmitter.defaultRecommendedMaximumEventListenersPerEventIdentifier property can be used. Note that this is not a hard limit.
+	EventEmitter instances will allow more functions to be bound but will output a warning indicating that a possible EventEmitter memory leak has been detected.
+	For any single EventEmitter, the eventEmitter.setRecommendedMaximumListenersPerEventIdentifier method can be used to avoid this warning.
+*/
+EventEmitter.defaultRecommendedMaximumEventListenersPerEventIdentifier = 10;
 
 // Export
 module.exports = EventEmitter;
--- system/events/tests/EventEmitterTest.js
+++ system/events/tests/EventEmitterTest.js
@@ -7,63 +7,109 @@ var Event = Framework.require('system/events/Event.js');
 // Class
 var EventEmitterTest = Test.extend({
 
+	// This should trigger a console warning
+	//testRecommendedMaximumBoundFunctionsPerEventIdentifier: function() {
+	//	var eventEmitter = new EventEmitter();
+	//	var functionsToBind = 11;
+	//	for(var i = 0; i < functionsToBind; i++) {
+	//		eventEmitter.on('event1', function() {
+	//		});
+	//	}
+	//},
+
 	testEventEmitter: function*() {
 		// Create the EventEmitter
 		var eventEmitter = new EventEmitter();
 
-		// List the event identifiers the EventEmitter handles
-		var eventIdentifiers = eventEmitter.getEventIdentifiers();
-		Assert.strictEqual(eventIdentifiers.length, 0, 'No event identifiers are set');
+		// List bound event listeners
+		var eventListeners = eventEmitter.getEventListeners();
+		//Console.info('eventListeners', eventListeners);
+		Assert.strictEqual(eventListeners.length, 0, 'No event listeners are bound');
 
 		// Variable used to store the event when it is emitted
 		var capturedEvent = null;
 
-		// Bind an event
-		eventEmitter.on('event1', function(event) {
+		// Declare the function to bind
+		var functionToBind = function(event) {
 			capturedEvent = event;
-		});
+		};
 
-		// List the event identifiers the EventEmitter handles
-		var eventIdentifiers = eventEmitter.getEventIdentifiers();
-		// TODO: more assertions
-		Assert.strictEqual(eventIdentifiers.length, 1, 'One event identifier is set');
+		// Bind the function to an event
+		eventEmitter.on('event1', functionToBind);
 
+		// List bound event listeners
+		eventListeners = eventEmitter.getEventListeners();
+		//Console.info('eventListeners', eventListeners);
+		Assert.strictEqual(eventListeners.length, 1, 'One event listener is bound');
+
+		// Emit an event
 		yield eventEmitter.emit('event1', 'event1Data');
-
 		Assert.true(Event.is(capturedEvent), 'Emitted an instance of Event');
 		Assert.strictEqual(capturedEvent.identifier, 'event1', 'identifier is set correctly');
 		Assert.strictEqual(capturedEvent.data, 'event1Data', 'data is set correctly');
 
-		//console.log('actual', actual);
+		// Unbind a specific function the event
+		eventEmitter.unbind('event1', functionToBind);
+
+		// List bound event listeners
+		eventListeners = eventEmitter.getEventListeners();
+		//Console.info('eventListeners', eventListeners);
+		Assert.strictEqual(eventListeners.length, 0, 'No event listeners are bound after unbind by specific function');
+
+		// Bind the event again
+		eventEmitter.on('event1', functionToBind);
+
+		// Unbind all event listeners from an event pattern
+		eventEmitter.unbind('event1');
+
+		// List bound event listeners
+		eventListeners = eventEmitter.getEventListeners();
+		//Console.info('eventListeners', eventListeners);
+		Assert.strictEqual(eventListeners.length, 0, 'No event listeners are bound after unbind by event pattern');
+
+		// Bind the event again
+		eventEmitter.on('event1', functionToBind);
+
+		// Unbind all bound event listeners
+		eventEmitter.unbindAll();
+
+		// List bound event listeners
+		eventListeners = eventEmitter.getEventListeners();
+		//Console.info('eventListeners', eventListeners);
+		Assert.strictEqual(eventListeners.length, 0, 'No event listeners are bound after unbindAll');
+	},
+
+	// If an EventEmitter does not have at least one listener registered for the 'error' event, and an 'error' event is emitted, the error is thrown, a stack trace is printed, and the Node.js process exits
+	//testEventEmitterThrowsErrorWithoutRegisteredListeners: function*() {
+	//	var eventEmitter = new EventEmitter();
+
+	//	yield Assert.throws(function*() {
+	//		yield eventEmitter.emit('event1', new Error());
+	//	}, 'Throw an error when an event is emitted with data being an instance of Error and there are no event listeners');
+	//},
+
+	testEventEmitterWithWildcardEventPatterns: function*() {
+	},
+
+	testEventEmitterWithRegularExpressionEventPatterns: function*() {
 	},
 
 	/*
-
-	If an EventEmitter does not have at least one listener registered for the 'error' event, and an 'error' event is emitted, the error is thrown, a stack trace is printed, and the Node.js process exits.
-const myEmitter = new MyEmitter();
-myEmitter.emit('error', new Error('whoops!'));
-  // Throws and crashes Node.js
-  When an EventEmitter instance experiences an error, the typical action is to emit an error event. Error events are treated as a special case. If there is no listener for it, then the default action is to print a stack trace and exit the program.
-
-  All EventEmitters emit the event 'newListener' when new listeners are added and 'removeListener' when a listener is removed.
-
-
-EventEmitter.defaultMaxListeners#
-By default, a maximum of 10 listeners can be registered for any single event. This limit can be changed for individual EventEmitter instances using the emitter.setMaxListeners(n) method. To change the default for all EventEmitter instances, the EventEmitter.defaultMaxListenersproperty can be used.
-Take caution when setting the EventEmitter.defaultMaxListeners because the change effects all EventEmitter instances, including those created before the change is made. However, calling emitter.setMaxListeners(n) still has precedence over EventEmitter.defaultMaxListeners.
-Note that this is not a hard limit. The EventEmitter instance will allow more listeners to be added but will output a trace warning to stderr indicating that a possible EventEmitter memory leak has been detected. For any single EventEmitter, the emitter.getMaxListeners() andemitter.setMaxListeners() methods can be used to temporarily avoid this warning:
-
+TODO:
+	
+	All EventEmitters emit the event 'newListener' when new listeners are added and 'removeListener' when a listener is removed.
+	
 	test .once
 
-	be able to list all of the events an emitter will emit and how many listeners they have
+	Fire an event N times and then remove it, an extension of the once concept.
+    server.on('foo', function() {
+      console.log('hello');
+    }, 4);
 
-	emitter.listeners(event)
-Returns an array of listeners for the specified event. This array can be manipulated, e.g. to remove listeners.
-    server.on('get', function(value) {
+	server.on('get', function(value) {
       console.log('someone connected!');
     });
     console.log(server.listeners('get')); // [ [Function] ]
-
 
 	Getting the actual event that fired.
     server.on('foo.*', function(event) {
@@ -71,11 +117,6 @@ Returns an array of listeners for the specified event. This array can be manipul
     });
 
     server.off (alias for removeListener)
-
-    Fire an event N times and then remove it, an extension of the once concept.
-    server.many('foo', 4, function() {
-      console.log('hello');
-    });
 
 	Namespaces with Wildcards To use namespaces/wildcards, pass the wildcard option into the EventEmitter constructor. When namespaces/wildcards are enabled, events can either be strings (foo.bar) separated by a delimiter or arrays (['foo', 'bar']). The delimiter is also configurable as a constructor option.
 
@@ -87,14 +128,6 @@ Returns an array of listeners for the specified event. This array can be manipul
 
     be able to supply a regex
     emitter.on(/myevent/g)
-
-    emitter.addListener(event, callback
-
-    removeListener
-removeAllListeners
-setMaximumListeners)
-
-	warn when too many listeners are bound
 
 	*/
 
