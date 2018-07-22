2467b09c8e7cb743e58199778b810dabeae54de4
kirkouimet <kirk@kirkouimet.com>
2018-07-22 12:47:25 -0600

    Adding tests for time

---
+++ globals/custom/tests/TimeTest.js
@@ -0,0 +1,19 @@
+// Dependencies
+import Test from 'framework/system/test/Test.js';
+import Assert from 'framework/system/test/Assert.js';
+
+// Class
+class TimeTest extends Test {
+
+	testTime() {
+		var time = new Time('1984-06-28 11:11 AM MST');
+		console.log(time);
+
+		Assert.notStrictEqual(time.dateObject, null, 'DateObject is set');
+		Assert.notStrictEqual(time.precision, null, 'Precision is set');
+	}
+
+}
+
+// Export
+export default TimeTest;
