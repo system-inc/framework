a263c7a6655f297a51f3a68c39448d730904c68b
kirkouimet <kirk@kirkouimet.com>
2020-08-26 13:09:27 -0600

    Starting work on carousel view

---
+++ system/interface/graphical/views/carousel/CarouselView.js
@@ -0,0 +1,14 @@
+// Dependencies
+import { View } from '@framework/system/interface/graphical/views/View.js';
+
+// Class
+class CarouselView extends View {
+
+	constructor() {
+		super();
+	}
+
+}
+
+// Export
+export { CarouselView };
